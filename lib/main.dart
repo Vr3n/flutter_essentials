a942bf6cac0c26e0b01531847b525a98448c9a95
vr3n <vickyspatel@gmail.com>
2022-02-19 19:35:59 +0530

    Changed to stateful Wisget and Added Assets

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,33 @@ void main() {
       theme: ThemeData(primarySwatch: Colors.orange)));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var my_text = "Change my name";
+  TextEditingController _nameController = TextEditingController();
+
+  // Listening to changes.
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _nameController.addListener(_setLatestValue);
+  // }
+
+  // @override
+  // void dispose() {
+  //   _nameController.dispose();
+  //   super.dispose();
+  // }
+
+  void _setLatestValue(text) {
+    my_text = _nameController.text;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +48,7 @@ class HomePage extends StatelessWidget {
       ),
       body: Center(
           child: Padding(
-        padding: const EdgeInsets.all(16),
+        padding: EdgeInsets.all(16),
         child: SingleChildScrollView(
           child: Card(
             child: Column(children: <Widget>[
@@ -33,15 +59,19 @@ class HomePage extends StatelessWidget {
                 height: 20,
               ),
               Text(
-                "Gaming",
+                my_text,
                 style: TextStyle(fontWeight: FontWeight.bold),
               ),
               SizedBox(
                 height: 20,
               ),
               Padding(
-                padding: const EdgeInsets.all(16.0),
+                padding: EdgeInsets.all(16.0),
                 child: TextField(
+                  controller: _nameController,
+                  onChanged: (text) {
+                    _setLatestValue(text);
+                  },
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     hintText: "Enter your name",
@@ -65,9 +95,6 @@ class HomePage extends StatelessWidget {
                       "https://static.wikia.nocookie.net/mrrobot/images/8/86/EdwardAlderson.jpg/revision/latest/top-crop/width/360/height/450?cb=20161117005643")),
             ),
             ListTile(
-              onTap: () {
-                print("Hello");
-              },
               leading: Icon(Icons.person),
               title: Text("Edward Alderson"),
               subtitle: Text('Father of elliot (Mr. Robot)'),
@@ -77,8 +104,11 @@ class HomePage extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        child: Icon(Icons.edit),
+        onPressed: () {
+          my_text = _nameController.text;
+          setState(() {});
+        },
+        child: Icon(Icons.send),
       ),
     );
   }
