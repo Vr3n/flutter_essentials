ffa5a0af8c290e9c6fedaec89e1c847bfc29f94c
vr3n <vickyspatel@gmail.com>
2022-02-19 16:07:17 +0530

    Understood the Concepts of Important Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,31 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Mr Robot Wiki'),
+      appBar: AppBar(
+        title: Text('Mr Robot Wiki'),
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          width: 100,
+          height: 100,
+          clipBehavior: Clip.antiAlias,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.black, Colors.blue]),
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 10)],
+          ),
+          child: Text(
+            'Elliot Alderson',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
         ),
-        body: Container(
-          child: Text('Elliot Alderson!'),
-        ));
+      ),
+    );
   }
 }
